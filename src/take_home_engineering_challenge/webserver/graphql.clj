b2ec1e102da8d8e3dea0c8f91dcf356e5c78d0fe
alfredoihernandez <a14hdz@gmail.com>
2020-06-17 11:15:31 -0500

    add functionality to foodTrucks call (filtering...

--- src/take_home_engineering_challenge/webserver/graphql.clj
+++ src/take_home_engineering_challenge/webserver/graphql.clj
@@ -10,11 +10,30 @@
   ;schema naming conventions: https://www.apollographql.com/docs/apollo-server/schema/schema/#:~:text=Naming%20conventions
   (slurp (:graphql-schema-path gather-configs)))
 
-(defrecord FoodTrucks []
+(defrecord FoodTrucks [user-latitude user-longitude filters order page-size page-idx]
   data/Resolvable
   (resolve!
     [_ _]
-    (foodtrucks/cached-data)))
+    (let [page-size-default (or page-size 5)
+          page-idx-default (or page-idx 0)
+          filters-default (or filters [])
+          order-default (keyword (or order "distance"))]
+      (log/info user-latitude)
+      (log/info user-longitude)
+      (log/info filters-default)
+      (log/info order-default)
+      (log/info page-size-default)
+      (log/info page-idx-default)
+      (-> (->> (foodtrucks/cached-data) ; get data
+               (map #(assoc % :distance ; calculate distance
+                            (foodtrucks/distance-between-two-coordinates
+                             (vals (select-keys % [:latitude :longitude]))
+                             [user-latitude user-longitude])))
+               (filter #(some? (:distance %))) ; filtering
+               (sort-by order-default) ; ordering
+               (partition-all page-size-default)) ; pagination
+          (nth page-idx-default)) ; pagination
+      )))
 
 (def QueryRoot
   {
